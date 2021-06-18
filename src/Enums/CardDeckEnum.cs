089839ce8ec21d5e2e323cc23c252efd3a04b290
Sevgin Hashimov <s.hashimov@fio.de>
2021-06-18 13:57:12 +0300

    remove 100

--- src/Enums/CardDeckEnum.cs
+++ src/Enums/CardDeckEnum.cs
@@ -3,7 +3,6 @@
     public enum CardDeckEnum
     {
         Zero = 0,
-        //Half = 1,
         One = 1,
         Two = 2,
         Three = 3,
@@ -12,8 +11,6 @@
         Thirteen = 13,
         Twenty = 20,
         Fourty = 40,
-        Hundred = 100,
         Question = 1000
-        //Coffee = 11,
     }
 }
